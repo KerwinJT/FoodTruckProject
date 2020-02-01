16d21f9f38d97148df8f154d41a57ce27f8ea849
KerwinJT <kerwinjt@gmail.com>
2020-02-01 14:39:53 -0700

    Food Truck Solutions - Draft

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,11 +1,52 @@
 package com.skilldistillery.foodtruck;
 
+// This class has no methods other than getters, setters, constructors, and an override of toString().
+// It is designed as a blueprint for individual objects of type FoodTruck
+// All interactions will be controlled through the main class - FoodTruckApp
+//
+
 public class FoodTruck {
 	private String name;
 	private String foodType;
 	private double rating;
 	private static int staticID;
 	private int assignedID;
+	
+	public FoodTruck() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public int getAssignedID() {
+		return assignedID;
+	}
+
+	public void setAssignedID(int assignedID) {
+		this.assignedID = assignedID;
+	}
 
 	@Override
 	public String toString() {
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -8,49 +8,121 @@ public class FoodTruckApp {
 		Scanner keyboard = new Scanner(System.in);
 		FoodTruck[] foodTruckArray; 
 		foodTruckArray = new FoodTruck[5];
-		foodTruckArray = generateFoodTrucks(keyboard, foodTruckArray);
-		printAllFoodTrucks(foodTruckArray);
+		FoodTruckApp initializeApp = new FoodTruckApp();
+		
+		System.out.println("This is the automated FoodTruck App.");
+		System.out.println("After input is complete, you will be able to display the stats of all trucks.");
+		foodTruckArray = initializeApp.generateFoodTrucks(keyboard, foodTruckArray);
+		initializeApp.appMenu(keyboard, foodTruckArray);
+		
 		// TODO Auto-generated method stub
 		keyboard.close();
 
 	}
-	public static FoodTruck[] generateFoodTrucks(Scanner keyboard, FoodTruck[] foodTruckArray) {
-		
+	public FoodTruck[] generateFoodTrucks(Scanner keyboard, FoodTruck[] foodTruckArray) {
+		FoodTruck[] customFoodTruck;
+		int arrayCounter = 0;
 		for (int i = 0; i < foodTruckArray.length; i++) {
-			System.out.print("Please provide the name of the Food Truck: ");
+			System.out.print("Please provide the name of the Food Truck or enter \"quit\": ");
 			String name = keyboard.nextLine();
+			if (name.contains("quit")) {
+				break;
+			}
 			System.out.print("Please provide the type of food for this truck: ");
 			String food = keyboard.nextLine();
 			System.out.print("Please provide the rating of this truck: ");
 			double rating = keyboard.nextDouble();
 			keyboard.nextLine();
 			foodTruckArray[i] = new FoodTruck(name, food, rating);
+			arrayCounter++;
 		}
-		return foodTruckArray;
+		customFoodTruck = new FoodTruck[arrayCounter];
+		for (int j = 0; j < foodTruckArray.length; j++) {
+			if (foodTruckArray[j] != null)
+				for (int y = 0; y < customFoodTruck.length; y++) {
+					if (customFoodTruck[y] == null) {
+						customFoodTruck[y] = foodTruckArray[j];
+						break;
+
+					}
+				}
+
+		}
+
+		return foodTruckArray = customFoodTruck;
 		
 	}
 	
-	public static void printAllFoodTrucks(FoodTruck[] foodTruckArray) {
+	public void printAllFoodTrucks(FoodTruck[] foodTruckArray) {
 		for (FoodTruck foodTruck : foodTruckArray) {
 			System.out.println(foodTruck.toString());
 		}
 	}
 	
-	public static void appMenu(Scanner keyboard) {
-		//Menus
-		//List food trucks
-		//Average rating display -- Complete method
-		//Display highest rating --  complete the method
-		//Quit program --- This can be completed in this method
+	public void appMenu(Scanner keyboard, FoodTruck[] foodTruckArray) {
+		int menuSelection = 0;
 		
-		//Make sure to loop this menu until "Quit" is inputed.
+		while (menuSelection != 4) {
+			System.out.println("Plese choose from the following menu (1 - 4):");
+			System.out.println("1. List Food Trucks.");
+			System.out.println("2. Diplay Average Rating.");
+			System.out.println("3. Display Highest Rated Food Truck(s).");
+			System.out.println("4. Quit.");
+			System.out.print("Selection: ");
+			menuSelection = keyboard.nextInt();
+			
+			switch (menuSelection){
+				case 1: {
+					printAllFoodTrucks(foodTruckArray);
+					break;
+				}
+				case 2: {
+					averageRating(foodTruckArray);
+					break;
+				}
+				case 3: {
+					highestRatedTrucl(foodTruckArray);
+					break;
+				}
+				case 4: {
+					
+					break;
+				}
+				default:
+					System.out.println("That was not a valid selection.");
+			}
+		}
+		//Menus -- finished
+		//List food trucks  -- finished
+		//Average rating display -- Complete method  -- Finished
+		//Display highest rating --  complete the method --Finished
+		//Quit program --- This can be completed in this method --- finished
+		
+		//Make sure to loop this menu until "Quit" is inputed.  --- finished
 	}
 	
-	public static void averageRating(FoodTruck[] foodTruckArray) {
+	public void averageRating(FoodTruck[] foodTruckArray) {
+		double average = 0;
+		for (FoodTruck foodTruck : foodTruckArray) {
+			average += foodTruck.getRating();
+		}
+		System.out.println("The average rating of the trucks is: " + ( average/ foodTruckArray.length));
 		//Average rating of the array
 	}
-	
-	public static void highestRatedTrucl(FoodTruck[] foodTruckArray) {
+
+	public void highestRatedTrucl(FoodTruck[] foodTruckArray) {
+		double highest = Double.MIN_VALUE;
+		FoodTruck bestFoodTruck = new FoodTruck();
+		if (foodTruckArray[0] != null) {
+			for (FoodTruck foodTruck : foodTruckArray) {
+				if (highest < foodTruck.getRating()) {
+					highest = foodTruck.getRating();
+					bestFoodTruck = foodTruck;
+				}
+			}
+		}
+		System.out.println("The highest rated FoodTruck is:");
+		System.out.println(bestFoodTruck.toString());
 		//highest rating of the food trucks
 	}
 	
